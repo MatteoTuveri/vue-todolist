6fc4070111a915d9c69ea51acbefb6691487be6c
MatteoTuveri <140177942+MatteoTuveri@users.noreply.github.com>
2023-10-31 16:50:28 +0100

    Milestone 1

--- JS/script.js
+++ JS/script.js
@@ -3,9 +3,29 @@ const { createApp } = Vue
 createApp({
   data() {
     return {
+        list:[
+            {
+                text :'Andare in palestra',
+                id : 1 ,
+                done : false
+            },
+            {
+                text :'Fare spesa',
+                id : 2 ,
+                done : false
+            },
+            {
+                text :'Fare il tagliando',
+                id : 3 ,
+                done : false
+            },
+        ]
     }
   },
   methods:{
+    check(index){
+        this.list[index].done = !this.list[index].done;
+    },
 
   }
 }).mount('#app')
